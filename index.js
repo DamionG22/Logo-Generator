ba399bd69ce6712db04085f1e408a8578891b0a7
blue butter <damiongallarza@gmail.com>
2023-06-28 20:33:33 -0700

    updates made to index, comments updated.

--- index.js
+++ index.js
@@ -3,92 +3,57 @@
 // ask questions
 
 // generate shape based on the questions and use fs to create the svg file
-const generateLogo = require('./lib/logo.js');
-const { Triangle, Circle, Square } = require('./lib/shapes');
 const fs = require('fs');
 const inquirer = require('inquirer');
-// function that prompts user with questions
-const promptUser = async () => {
-    // Use Inquirer to prompt the user for input
-    const { text, textColor, shape, shapeColor } = await inquirer.prompt([
-      {
-        type: 'input',
-        name: 'text',
-        message: 'Enter the text for the logo (up to three characters):',
-        validate: (input) => {
-          // Validate that the input is not empty and has a maximum length of three characters
-          if (input.trim() !== '' && input.length <= 3) {
-            return true;
-          }
-          return 'Please enter up to three characters for the logo text.';
-        },
-      },
-      {
-        type: 'input',
-        name: 'textColor',
-        message: 'Enter the color for the logo text (color keyword or hexadecimal number):',
-      },
-      {
-        type: 'list',
-        name: 'shape',
-        message: 'Select a shape for the logo:',
-        choices: ['circle', 'triangle', 'square'],
-      },
-      {
-        type: 'input',
-        name: 'shapeColor',
-        message: 'Enter the color for the logo shape (color keyword or hexadecimal number):',
-      },
-    ]);
-  
-    // Return the user's input
-    return { text, textColor, shape, shapeColor };
-  };
-  const generateLogo = ({ text, textColor, shape, shapeColor }) => {
-    let logo;
-  
-    // Create an instance of the corresponding shape class based on the user's selection
-    if (shape === 'circle') {
-      logo = new Circle();
-    } else if (shape === 'triangle') {
-      logo = new Triangle();
-    } else if (shape === 'square') {
-      logo = new Square();
+const { Circle, Triangle, Square } = require('./lib/shapes');
+const generateLogo = require('./lib/logo');
+
+// Define the prompts for user input
+const prompts = [
+  {
+    type: 'input',
+    name: 'text',
+    message: 'Enter the text for the logo (up to three characters):',
+    validate: function (input) {
+      if (input.length > 3) {
+        return 'Please enter up to three characters.';
+      }
+      return true;
     }
-  
-    // Set the color for the shape and text
-    logo.setColor(shapeColor);
-    logo.setTextColor(textColor);
-  
-    // Generate the SVG string for the logo
-    const svg = logo.render(text);
-  
-    // Return the SVG string
-    return svg;
-  };
+  },
+  {
+    type: 'input',
+    name: 'textColor',
+    message: 'Enter the text color:',
+    default: 'black'
+  },
+  {
+    type: 'list',
+    name: 'shape',
+    message: 'Select a shape:',
+    choices: ['Circle', 'Triangle', 'Square']
+  },
+  {
+    type: 'input',
+    name: 'shapeColor',
+    message: 'Enter the shape color:',
+    default: 'black'
+  }
+];
 
-  const saveLogo = (svg) => {
-    // Write the SVG string to a file named "logo.svg"
-    const fs = require('fs');
-    fs.writeFileSync('logo.svg', svg);
-  
-    // Display success message in the command line
-    console.log('Generated logo.svg');
-  };
-  
-  const run = async () => {
-    // Prompt the user for input
-    const userInput = await promptUser();
-  
-    // Generate the logo SVG
-    const logoSvg = generateLogo(userInput);
-  
-    // Save the logo SVG as logo.svg
-    saveLogo(logoSvg);
-  };
-  
-  // Call the run function to start the application
-  run();
-  
+// Handle the user's responses
+function handleUserInput(answers) {
+  const { text, textColor, shape, shapeColor } = answers;
+  const svgContent = generateLogo(text, textColor, shape, shapeColor);
 
-  
+  fs.writeFile('logo.svg', svgContent, function (err) {
+    if (err) {
+      console.log('An error occurred while generating the SVG file.');
+      console.error(err);
+    } else {
+      console.log('Generated logo.svg');
+    }
+  });
+}
+
+inquirer.prompt(prompts).then(handleUserInput);
